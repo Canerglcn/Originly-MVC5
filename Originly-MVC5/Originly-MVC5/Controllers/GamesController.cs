2d81914ceafb1ce60e8fc94b9f58a0b6e2e28602
Caner <canerglcn@gmail.com>
2020-02-29 19:06:48 +0300

    project added

---
+++ Originly-MVC5/Originly-MVC5/Controllers/GamesController.cs
@@ -0,0 +1,40 @@
+﻿using Originly_MVC5.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Originly_MVC5.Controllers
+{
+    public class GamesController : Controller
+    {
+        // GET: Games
+        public ViewResult Index()
+        {
+            var games = GetGames();
+
+            return View(games);
+
+        }
+
+        public ActionResult Details(int id)
+        {
+            var game = GetGames().SingleOrDefault(c => c.Id == id);
+            if (game == null)
+                return HttpNotFound();
+
+            return View(game);
+        }
+
+
+        private IEnumerable<Game> GetGames()
+        {
+            return new List<Game>
+            {
+                new Game{Id=1,Name="GTA5"},
+                new Game{Id=2,Name="Metin2"}
+            };
+        }
+    }
+}
---
+++ Originly-MVC5/Originly-MVC5/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Originly_MVC5.MvcApplication" Language="C#" %>
