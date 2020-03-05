51ae20a9a4ac1df04b033951b3eadffce69da968
Caner <canerglcn@gmail.com>
2020-03-05 23:44:06 +0300

    add ValidationAdded

--- Originly-MVC5/Originly-MVC5/Controllers/CustomersController.cs
+++ Originly-MVC5/Originly-MVC5/Controllers/CustomersController.cs
@@ -34,6 +34,17 @@ namespace Originly_MVC5.Controllers
         [HttpPost]
         public ActionResult Save(Customer customer)
         {
+            if(!ModelState.IsValid)
+            {
+                var viewModel = new CustomerFormViewModel
+                {
+                    Customer = customer,
+                    MembershipTypes = _context.MembershipTypes.ToList()
+                };
+                
+                 return View("CustomerForm", viewModel);
+            }
+
             if(customer.Id==0)
                  _context.Customers.Add(customer);
             else
